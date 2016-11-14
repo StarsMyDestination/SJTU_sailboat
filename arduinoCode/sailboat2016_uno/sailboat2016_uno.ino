8c6cf2d14761c48bc6898a5a90514688aa3a961f
StarsMyDestination <xujianyun101@gmail.com>
2016-11-14 22:33:26 +0800

    ver1.1 implement parse function

--- arduinoCode/sailboat2016_uno/sailboat2016_uno.ino
+++ arduinoCode/sailboat2016_uno/sailboat2016_uno.ino
@@ -5,39 +5,7 @@ int GPSAddress = 0x42;//GPS I2C Address
 String gpsString = "";
 double originLat = 31.0326 / 180.0 * pi, originLon = 121.44162 / 180.0 * pi;
 double listNE[2];
-
-
-double Datatransfer(char *data_buf, char num) //Data type converter：convert char type to float
-{ //*data_buf:char data array ;num:float length
-  double temp = 0.0;
-  unsigned char i, j;
-
-  if (data_buf[0] == '-') //negative number
-  {
-    i = 1;
-    //char to int and add
-    while (data_buf[i] != '.')
-      temp = temp * 10 + (data_buf[i++] - 0x30);
-    for (j = 0; j < num; j++)
-      temp = temp * 10 + (data_buf[++i] - 0x30);
-    //int to float
-    for (j = 0; j < num; j++)
-      temp = temp / 10;
-    //to negative number
-    temp = 0 - temp;
-  }
-  else//positive number
-  {
-    i = 0;
-    while (data_buf[i] != '.')
-      temp = temp * 10 + (data_buf[i++] - 0x30);
-    for (j = 0; j < num; j++)
-      temp = temp * 10 + (data_buf[++i] - 0x30);
-    for (j = 0; j < num; j++)
-      temp = temp / 10 ;
-  }
-  return temp;
-}
+double lat, lon;
 
 
 void recvInit() {
@@ -76,8 +44,36 @@ void recvFromGps() {
 }
 
 
-void dataParser() {
-
+int dataParser() {
+  if (gpsString.startsWith("$GPGGA")) {
+    int commaIndexList[20] = {0};
+    int commaCount = 1;
+    while (1) {
+      int tmpIndex = gpsString.indexOf(',', commaIndexList[commaCount - 1] + 1);
+      if (tmpIndex == -1) break;
+      commaIndexList[commaCount] = tmpIndex;
+      //      Serial.println(commaIndexList[commaCount]);
+      commaCount ++;
+    }
+    // latitude
+    String latStr = "";
+    for (int i = commaIndexList[2] + 1; i < commaIndexList[3]; i++) {
+      latStr += gpsString[i];
+    }
+    Serial.println("latStr: " + latStr);
+    lat = latStr.toFloat();
+    lat = dm2dd(lat);
+    // longtitude
+    String lonStr = "";
+    for (int i = commaIndexList[4] + 1; i < commaIndexList[5]; i++) {
+      lonStr += gpsString[i];
+    }
+    Serial.println("latStr: " + lonStr);
+    lon = lonStr.toFloat();
+    lon = dm2dd(lon);
+    return 1;
+  }
+  else return 0;
 }
 
 
@@ -97,24 +93,46 @@ void w84ToNE(double lat, double lon, double NE[]) {
   NE[1] = r2 * cos(originLat) * d_lon;
 }
 
+
+double dm2dd(double data) {
+  double tmp = data / 100.0;
+  double d = floor(tmp);
+  double m = 100*(tmp - d);
+  return d + m / 60.0;
+}
+
 void setup()
 {
   Wire.begin();
-  Serial.begin(9600);
-  Serial.println("DFRobot DFRduino GPS Shield v1.0");
-  Serial.println("$GPGGA statement information: ");
+  Serial.begin(115200);
 }
+
+// void test(int a[]) {
+//   a[0] = 100;
+// }
 
 void loop()
 {
-  //  recvFromGps();
-  //  Serial.println(gpsString);
-  w84ToNE(31.0326, 121.44169, listNE);
-  Serial.println(listNE[0], 8);
-  Serial.println(listNE[1], 8);
-  delay(1000);
+  recvFromGps();
+  Serial.println(gpsString);
+  if (dataParser()) {
+    w84ToNE(lat, lon, listNE);
+    Serial.println(lat, 5);
+    Serial.println(lon, 5);
+    Serial.println(listNE[0], 4);
+    Serial.println(listNE[1], 4);
+  }
+  //  char *buff[] = {"A","B"};
+  //  *buff[0] = 'A';
+  //  Serial.println(buff[0]);
+  //  int b[2];
+  //  test(b);
+  //  Serial.println(b[0]);
+  // int *p;
+  // p[0]=1; p[1]=3;
+  // Serial.println(*(p+1));
+  //  String a = "-12.3";
+  //  Serial.println(a.toFloat());
+  //  delay(500);
 
-  //String aa = '$';
-  //Serial.println(aa);
-  //delay(500);
 }
